1580bae5adcbdb89e74b1e247eff48a3ebf70b95
Uralbek <oralbekergenov@gmail.com>
2022-07-28 23:27:00 +0500

    Create playRound function
    The logic of that function is simply.
    It's decide who is the winnder,
    based on your and computer choice.
    
    1. Rock Beats Scissors
    2. Scissors Beats Paper
    3. Paper Beats Rock

--- script.js
+++ script.js
@@ -4,3 +4,49 @@ function getComputerChoice() {
 
     return arr[randomIndex];
 }
+
+function playRound(playerSelection, computerSelection) {
+    let isDraw = false;
+    let isWon = false;
+    playerSelection = playerSelection.toLowerCase();
+    
+    // rock beats scissors
+    // scissors beats paper
+    // paper beats rock
+    // if two of us picked same thing then Tie
+
+    if (playerSelection !== computerSelection) {
+
+        switch (playerSelection) {
+
+            // Rock
+            case "rock":
+                if (computerSelection === "scissors")
+                    isWon = true;
+
+                break;
+            
+            // Paper
+            case "paper":
+                if (computerSelection === "rock")
+                    isWon = true;
+
+                break;
+
+            // Scissors
+            case "scissors":
+                if (computerSelection === "paper")
+                    isWon = true;
+
+                break;
+        }
+    } else {
+        isDraw = true;
+    }
+
+    let message = (isDraw) ? "It's Draw!" :
+        (isWon) ? `You Win! ${playerSelection} beats ${computerSelection}` :
+        `You Lose! ${computerSelection} beats ${playerSelection}`;
+
+    return message;
+}
